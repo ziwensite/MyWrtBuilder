57fb0afa56b0063b398740844fc03603cd8a7eba
ziwensite <yuanliangsong@outlook.com>
2024-11-18 23:59:00 +0800

    Update builder-patch.sh

--- scripts/builder-patch.sh
+++ scripts/builder-patch.sh
@@ -14,6 +14,24 @@ sed -i "s/install \$(BUILD_PACKAGES)/install \$(BUILD_PACKAGES) --force-overwrit
 # Not generate ISO images for it is too big
 sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
 
+# Not generate VDI images for it is too big
+sed -i "s/CONFIG_VDI_IMAGES=y/# CONFIG_VDI_IMAGES is not set/" .config
+
+# Not generate QCOW2 images for it is too big
+sed -i "s/CONFIG_QCOW2_IMAGES=y/# CONFIG_QCOW2_IMAGES is not set/" .config
+
+# Not generate GZIP images for it is too big
+sed -i "s/CONFIG_TARGET_IMAGES_GZIP=y/# CONFIG_TARGET_IMAGES_GZIP is not set/" .config
+
+# Not generate ROOTFS_CPIOGZ images for it is too big
+sed -i "s/CONFIG_TARGET_ROOTFS_CPIOGZ=y/# CONFIG_TARGET_ROOTFS_CPIOGZ is not set/" .config
+
+# Not generate ROOTFS_TARGZ images for it is too big
+sed -i "s/CONFIG_TARGET_ROOTFS_TARGZ=y/# CONFIG_TARGET_ROOTFS_TARGZ is not set/" .config
+
+# Not generate ROOTFS_SQUASHFS images for it is too big
+sed -i "s/CONFIG_TARGET_ROOTFS_SQUASHFS=y/# CONFIG_TARGET_ROOTFS_SQUASHFS is not set/" .config
+
 # Not generate VHDX images
 # sed -i "s/CONFIG_VHDX_IMAGES=y/# CONFIG_VHDX_IMAGES is not set/" .config
 

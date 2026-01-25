64a88bd3e37b1e2b1c9c61fb2c4f779e1e2e2aa9
ziwensite <yuanliangsong@outlook.com>
2026-01-25 22:36:29 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-filebrowser-go-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-cifs-mount-zh-cn cifsmount"
 PACKAGES="$PACKAGES unishare luci-app-unishare"
-PACKAGES="$PACKAGES unishare luci-app-partexp"
+PACKAGES="$PACKAGES luci-app-partexp"
 
 # Diskman 磁盘管理
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
